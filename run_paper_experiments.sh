5a41a5e4d311de748d5e602bfff86a8fa44ec852
Leonardo Pepino <leonardodpepino@gmail.com>
2021-04-11 17:13:26 -0300

    Update run_paper_experiments.sh

--- run_paper_experiments.sh
+++ run_paper_experiments.sh
@@ -21,12 +21,20 @@ paiprun configs/main/spectrogram-baseline.yaml --output_path "s3://lpepino-datas
 errors=$?; done
 #-----------------------------------------Wav2Vec2-PT-----------------------------------------------
 #Local Encoder
-#paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/w2v2PT-local/${SEED}" --mods $seeds_mod"&global/wav2vec2_embedding_layer=local_encoder&global/dienen_config=!yaml configs/dienen/mean_mlp.yaml"
+errors=1
+while (($errors!=0)); do
+paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/using_dropout/w2v2PT-local/${SEED}" --mods "${seed_mod}&global/wav2vec2_embedding_layer=local_encoder&global/dienen_config=!yaml configs/dienen/mean_mlp.yaml"
+errors=$?; done
 #Contextual Encoder
-#paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/w2v2PT-contextual/${SEED}" --mods $seeds_mod"&global/wav2vec2_embedding_layer=output&global/dienen_config=!yaml configs/dienen/mean_mlp.yaml"
+errors=1
+while (($errors!=0)); do
+paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/using_dropout/w2v2PT-contextual/${SEED}" --mods "${seed_mod}&global/wav2vec2_embedding_layer=output&global/dienen_config=!yaml configs/dienen/mean_mlp.yaml"
+errors=$?; done
 #All Layers
-#paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/w2v2PT-alllayers/${SEED}" --mods $seeds_mod"&global/wav2vec2_embedding_layer=enc_and_transformer"
-
+errors=1
+while (($errors!=0)); do
+paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/using_dropout/w2v2PT-alllayers/${SEED}" --mods "${seed_mod}&global/wav2vec2_embedding_layer=enc_and_transformer"
+errors=$?; done
 #-----------------------------------------Wav2Vec2-LS960---------------------------------------------
 ls960_mod="&global/wav2vec2_model_path=~/Models/wav2vec2/wav2vec_small_960h.pt&global/wav2vec2_dict_path=~/Models/wav2vec2"
 #Local Encoder
