c4aa28f9748a99928b204ecea16edcf4d5a0d208
Leonardo Pepino <leonardodpepino@gmail.com>
2021-04-11 17:10:01 -0300

    Update run_paper_experiments.sh

--- run_paper_experiments.sh
+++ run_paper_experiments.sh
@@ -10,10 +10,15 @@ seed_mod="global/seed=${SEED}"
 
 #-----------------------------------------Baseline features-----------------------------------------
 #OS-experiment
-#paiprun configs/main/os-baseline.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/os-baseline/${SEED}" --mods $seed_mod
+errors=1
+while (($errors!=0)); do
+paiprun configs/main/os-baseline.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/using_dropout/os-baseline/${SEED}" --mods $seed_mod
+errors=$?; done
 #Spectrogram-experiment
-#paiprun configs/main/spectrogram-baseline.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/spectrogram-baseline/${SEED}" --mods $seed_mod
-
+errors=1
+while (($errors!=0)); do
+paiprun configs/main/spectrogram-baseline.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/using_dropout/spectrogram-baseline/${SEED}" --mods $seed_mod
+errors=$?; done
 #-----------------------------------------Wav2Vec2-PT-----------------------------------------------
 #Local Encoder
 #paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/w2v2PT-local/${SEED}" --mods $seeds_mod"&global/wav2vec2_embedding_layer=local_encoder&global/dienen_config=!yaml configs/dienen/mean_mlp.yaml"
@@ -52,14 +57,17 @@ while (($errors!=0)); do
 paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/using_dropout/w2v2PT-alllayers-globalnorm/${SEED}" --mods "${seed_mod}&global/wav2vec2_embedding_layer=enc_and_transformer&global/normalize=global"
 errors=$?; done
 #+eGeMAPS
-#paiprun experiments/configs/main/w2v2-os-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/w2v2PT-alllayers-egemaps/${SEED}" --mods $seeds_mod"global/wav2vec2_embedding_layer=enc_and_transformer&global/normalize=global"
+errors=1
+while (($errors!=0)); do
+paiprun configs/main/w2v2-os-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/using_dropout/w2v2PT-alllayers-egemaps/${SEED}" --mods "${seed_mod}&global/wav2vec2_embedding_layer=enc_and_transformer&global/normalize=global"
+errors=$?; done
 
 #---------------------------------------SOA Paper Setting---------------------------------------------
 #Ravdess
-#errors=1
-#while (($errors!=0)); do
-#paiprun configs/main/w2v2-soapaper-config.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/using_dropout/soapaper-ravdess/${SEED}" --mods "${seed_mod}&global/wav2vec2_embedding_layer=enc_and_transformer&global/normalize=global&global/dataset=ravdess"
-#errors=$?; done
+errors=1
+while (($errors!=0)); do
+paiprun configs/main/w2v2-soapaper-config.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/using_dropout/soapaper-ravdess/${SEED}" --mods "${seed_mod}&global/wav2vec2_embedding_layer=enc_and_transformer&global/normalize=global&global/dataset=ravdess"
+errors=$?; done
 #IEMOCAP
 errors=1
 while (($errors!=0)); do
