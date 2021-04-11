05f3932f1fb66790b2fa9faac21d1078fac56e73
Leonardo Pepino <leonardodpepino@gmail.com>
2021-04-11 16:59:22 -0300

    Update run_paper_experiments.sh

--- run_paper_experiments.sh
+++ run_paper_experiments.sh
@@ -23,14 +23,22 @@ seed_mod="global/seed=${SEED}"
 #paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/w2v2PT-alllayers/${SEED}" --mods $seeds_mod"&global/wav2vec2_embedding_layer=enc_and_transformer"
 
 #-----------------------------------------Wav2Vec2-LS960---------------------------------------------
-#ls960_mod = "&global/wav2vec2_model_path=~/Models/wav2vec2/wav2vec_small_960h.pt&global/wav2vec2_dict_path=~/Models/wav2vec2"
-
+ls960_mod="&global/wav2vec2_model_path=~/Models/wav2vec2/wav2vec_small_960h.pt&global/wav2vec2_dict_path=~/Models/wav2vec2"
 #Local Encoder
-#paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/w2v2LS960-local/${SEED}" --mods $seeds_mod"&global/wav2vec2_embedding_layer=local_encoder&global/dienen_config=!yaml configs/dienen/mean_mlp.yaml"$ls960_mod
+errors=1
+while (($errors!=0)); do
+paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/using_dropout/w2v2LS960-local/${SEED}" --mods "${seed_mod}&global/wav2vec2_embedding_layer=local_encoder&global/dienen_config=!yaml configs/dienen/mean_mlp.yaml$ls960_mod"
+errors=$?; done
 #Contextual Encoder
-#paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/w2v2LS960-contextual/${SEED}" --mods $seeds_mod"&global/wav2vec2_embedding_layer=output&global/dienen_config=!yaml configs/dienen/mean_mlp.yaml"$ls960_mod
+errors=1
+while (($errors!=0)); do
+paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/using_dropout/w2v2LS960-contextual/${SEED}" --mods "${seed_mod}&global/wav2vec2_embedding_layer=output&global/dienen_config=!yaml configs/dienen/mean_mlp.yaml$ls960_mod"
+errors=$?; done
 #All Layers
-#paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/w2v2LS960-alllayers/${SEED}" --mods $seeds_mod"&global/wav2vec2_embedding_layer=enc_and_transformer"$ls960_mod
+errors=1
+while (($errors!=0)); do
+paiprun configs/main/w2v2-exps.yaml --output_path "s3://lpepino-datasets2/is2021_experiments/using_dropout/w2v2LS960-alllayers/${SEED}" --mods "${seed_mod}&global/wav2vec2_embedding_layer=enc_and_transformer$ls960_mod"
+errors=$?; done
 
 #------------------------------------------Ablation--------------------------------------------------
 #LSTM
