73822bdb0e337cc6dbe3b67d5d1b1ed2dc889536
Leonardo Pepino <leonardodpepino@gmail.com>
2021-04-22 17:04:16 -0300

    run just 5 seeds

--- run_seeds.sh
+++ run_seeds.sh
@@ -3,6 +3,6 @@ set -x
 
 OUTPUT_PATH=$1
 
-for seed in 0123 1234 2345 3456 4567 5678 6789 7890 8901 9012; do
+for seed in 0123 1234 2345 3456 4567; do
     ./run_paper_experiments.sh $seed $OUTPUT_PATH
     done
